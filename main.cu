ce3c32ae429a11ef286d9f4b0e4ebac977d11f39
Vu <vuanh@wang.cs.grinnell.edu>
2023-11-25 16:43:44 -0600

    testing

--- main.cu
+++ main.cu
@@ -13,10 +13,10 @@ extern "C" {
 int main() {
   srand(time(NULL));
 
-  int rows_A = 16384;
-  int cols_A = 1024;
-  int rows_B = 1024;
-  int cols_B = 32768;
+  int rows_A = 2048;
+  int cols_A = 4;
+  int rows_B = 4;
+  int cols_B = 2048;
 
   float *A = (float *) malloc(sizeof(float) * (rows_A * cols_A));
   float *B = (float *) malloc(sizeof(float) * (rows_B * cols_B));
@@ -26,11 +26,11 @@ int main() {
   fill_random_floats(A, rows_A * cols_A);
   fill_random_floats(B, rows_B * cols_B);
 
-  // // CPU
-  // clock_t start_cpu = clock();
-  // cpu__matrix_multiply(A, B, cpu_result, rows_A, cols_A, rows_B, cols_B);
-  // clock_t end_cpu = clock();
-  // printf("CPU: %.3f\n", (double)(end_cpu - start_cpu) / CLOCKS_PER_SEC);
+  // CPU
+  clock_t start_cpu = clock();
+  cpu__matrix_multiply(A, B, cpu_result, rows_A, cols_A, rows_B, cols_B);
+  clock_t end_cpu = clock();
+  printf("CPU: %.3f\n", (double)(end_cpu - start_cpu) / CLOCKS_PER_SEC);
 
   // GPU
   clock_t start_gpu = clock();
